c5a192d3c743ea77af531279319cf03f76e10953
Eric Bréchemier <github@eric.brechemier.name>
2006-10-26 22:21:51 +0000

    Updated to Shibuya version

--- main/src/java/net/sf/tokyo/ITokyoNaut.java
+++ main/src/java/net/sf/tokyo/ITokyoNaut.java
@@ -22,181 +22,107 @@
 package net.sf.tokyo;
 
 /**
- * ITokyoNaut is the single interface of TokyoNautilus API<br />
+ * ITokyoNaut is the one single interface making up TokyoNautilus API<br />
  *
  * The TokyoNautilus API enables both XML and non-XML data browsing through a concise API. Nautilus is a 
  * reference to Captain Nemo's submarine in "Twenty Thousand Leagues Under the Sea" by Jules Verne.
- * Captain Nemo's motto, "Mobilis in Mobili", fits well with this flexible data navigation API. 
+ * Captain Nemo's motto, "Mobilis in Mobili", fits well with this flexible data navigation API.
  * 
  * @author Eric Br&eacute;chemier
- * @version Version One
+ * @version Shibuya
  */
 public interface ITokyoNaut
 {
-  /**
-   * Delegate some parts of the data handling with a "collaborator", as specified by "role".<br/>
-   *
-   * <p>
-   * This method allows to combine different INautilus in a flexible way, 
-   * each responsible for a different part of the data extraction, e.g.:
-   *   <ul>
-   *     <li>Query: XPath parsing and interpretation</li>
-   *     <li>Grammar defining the data structure</li>
-   *     <li>Data parsing and aggregation into result</li>
-   *   </ul>
-   * </p>
-   *
-   * <p>Null Implementation:</p>
-   * <pre>
-   * return collaborator;
-   * </pre>
-   *
-   * @param collaborator complementary TokyoNaut
-   * @param role implementation-specific role, identifying the type of collaboration involved. The same instance can be plugged several times with different roles.
-   * @return return collaborator to allow further combinations on the same line of code
-   */
-  public ITokyoNaut plug(ITokyoNaut collaborator, Object role);
   
   /**
-   * Make "collaborator" redundant to free resources... ;(<br/>
+   * Every data processing starts with such a meeting between two TokyoNaut instances.<br/>
    *
    * <p>
-   * This method should be called after a successful call to plug() with the same arguments.
-   * It allows to remove a previously created association, e.g. in order to free resources.
+   * The stranger instance is to propose a language stored in context[language], with
+   * type[language] characterizing the language Object using a shared convention. 
+   * The hearer TokyoNaut may either say something using the language, or keep silent 
+   * and quit, using the same context, type and language as arguments.
+   * </p>
+   * <p>
+   *  <b>Note:</b> TokyoNaut API makes extensive use of the three parameters <br/>
+   *  Object[] context + int[] type + int someObjectPosition. <br/>
+   *  The combination of context and type can be seen as a stack of named arguments,
+   *  while the last integer parameter points to the top of the stack. 
+   *  The rule is rather to keep the same arrays for the duration of the conversation,
+   *  even if bigger arrays may be introduced as required if shared conventions associated 
+   *  with the current language allow it. Besides, the two arrays should have the same size.  
    * </p>
    *
-   * <p>Null Implementation:</p>
-   * <pre>
-   * return this;
-   * </pre>
-   *
-   * @param collaborator complementary TokyoNaut
-   * @param role implementation-specific role, identifying the type of collaboration involved. If the same instance has be plugged several times with different roles, it needs to be unplugged for each role to be freed.
-   * @return current TokyoNaut to allow new combinations on the same line of code
+   * @param stranger TokyoNaut instance coming to start a conversation
+   * @param context stack of questions and answers
+   * @param type array of codes identifying types of objects at the same position in context array
+   * @param language position of language object in context array
    */
-  public ITokyoNaut unplug(ITokyoNaut collaborator, Object role);
+  public void meet(ITokyoNaut stranger, Object[] context, int[] type, int language);
   
   /**
-   * Map given path, storing locations found in given buffer<br/>
+   * At the end of all discussions, TokyoNauts part company.<br/>
    *
    * <p>
-   * Scouts for data items on selection "path", and stores their locations in "buffer".
-   * To allow repeated calls of this method, the first items are ignored 
-   * until the "from" position is reached, counting from 1 like XPath position().
-   * Including "from", at most "length" items are then stored in "buffer".
-   * The "buffer" can be filled in several steps, by setting the "offset" to
-   * the starting position for the storage of new items. It corresponds to 
-   * the sum of values returned in previous calls, since this method 
-   * returns the actual number of items stored at each step.
-   * ITokyoNaut implementations should at least support "/","*","@*" for "path", 
-   * similarly to their XPath meaning in XML Documents.
+   * This is the occasion
+   * to free contextual ressources related to the task. Any TokyoNaut may end the discussion,
+   * not necessarily the one having started it by calling the meet method.
+   * context[language] and type[language] will usually be identical to the values
+   * provided in the initial meet method.
    * </p>
    *
-   * <p>Null Implementation:</p>
-   * <pre>
-   * return 0;
-   * </pre>
-   *
-   * @param path path to follow
-   * @param from 1-based position of first item to be taken into account on path
-   * @param buffer buffer to store selected items
-   * @param offset 0-based offset to start storing items in buffer
-   * @param length number of items to be selected on path
-   * @return the actual number of items stored in buffer
+   * @param friend TokyoNaut instance wishing to quit the conversation
+   * @param context stack of questions and answers
+   * @param type array of codes identifying types of objects at the same position in context array
+   * @param language position of language object in context array
    */
-  public int map(Object path, int from, Object[] buffer, int offset, int length);
+  public void quit(ITokyoNaut friend, Object[] context, int[] type, int language);
   
   /**
-   * Jump to first item found on given path and return updated self<br/>
+   * TokyoNauts instances communicate by calling alternatively this "say" method.<br/>
    *
    * <p>
-   * Jump to a well-suited position for reading the first item on given "path",
-   * starting from here() excluded.
-   * Return this TokyoNaut to allow further moves on the same line of code.
-   * Sets current location to "nowhere" (here() returns null)
-   * when this move is not supported or no data item is found.
-   * ITokyoNaut implementations should at least support "/","*","@*" for "path", 
-   * similarly to their XPath meaning in XML Documents.
+   * The speaker TokyoNaut says context[sentence], characterized by type[sentence], 
+   * assuming it is relevant in the context. 
+   * Each TokyoNaut is free to update the context stack to keep only the most relevant items. 
+   * For hierarchical data processing, the context may for example contain the stack of
+   * ancestors on the path from the root to the current node.
    * </p>
    *
-   * <p>Null Implementation:</p>
-   * <pre>
-   * return this;
-   * </pre>
-   *
-   * @param path path to follow
-   * @return TokyoNaut positionned on first item reached by following "path" from previous location.
+   * @param speaker TokyoNaut instance uttering the sentence
+   * @param context stack of questions and answers
+   * @param type array of codes identifying types of objects at the same position in context array
+   * @param sentence position of sentence object in context array
    */
-  public ITokyoNaut go(Object path);
+  public void say(ITokyoNaut speaker, Object[] context, int[] type, int sentence);
   
   /**
-   * Get current location<br/>
+   * When tired of speaking, TokyoNauts can enjoy a little distraction...<br/>
    *
    * <p>
-   * Can be used for bookmarking, by storing the returned location and later 
-   * calling go(location); however, in mutable data structures, there is
-   * no warranty that the corresponding data item will be exactly the same, 
-   * or even still exist.
-   * Return null if previous call to go() led to "nowhere", as well as initially 
-   * before any call to go().
-   * In ITokyoNaut implementations, when here() is not null, calling here().toString() 
-   * should return an absolute path, either in XPath in the context of a single document,
-   * or in XPointer to also include current document's URI.
+   * ... by playing
+   * a quick game, a piece of music or even theater, proposed as context[play]. 
+   * Serious TokyoNauts can freely skip the implementation of this method.
    * </p>
-   * 
-   * <p>Null Implementation:</p>
-   * <pre>
-   * return null;
-   * </pre>
    *
-   * @return current location
-   */
-  public Object here();
+   * @param player TokyoNaut instance communicating through play
+   * @param context stack of questions and answers and various plays
+   * @param type array of codes identifying types of objects at the same position in context array
+   * @param play position of play object in context array
+   */ 
+  public void play(ITokyoNaut player, Object[] context, int[] type, int play);
   
   /**
-   * Get the value of the first item found on given path<br/>
+   * No need to panic, says Japan coach.<br/>
    *
    * <p>
-   * Return the value of the first item found on given "path".
-   * Return null when this move is not supported or no data item is found.
-   * ITokyoNaut implementations should at least support "." for "path",
-   * in the context of simple data items with text values,
-   * similarly to its XPath meaning in XML Documents.
+   * Gestures provide alternative communication, especially useful for logs, 
+   * assertions and errors.
    * </p>
    *
-   * <p>Null Implementation:</p>
-   * <pre>
-   * return null;
-   * </pre>
-   *
-   * @param path path to follow
-   * @return value of first item found or null if this move is not supported or no data item is found
-   */
-  public Object get(Object path);
-  
-  /**
-   * Put given item to the first position on given path<br/>
-   *
-   * <p>
-   * Put given "item" at the first position on given "path", 
-   * replacing any previous data item found there.
-   * Returns null if this "path" is not supported or the data structure is immutable.
-   * Can be used to delete data items by setting "item" to null.
-   * Insertion before, between, or after existing data items may be supported by 
-   * refining on the "path", e.g. inserting "+" or "-" after positions as in
-   * "/Root/Element[3+]" or "/Root/Element[1-]".
-   * ITokyoNaut implementations should only support the put() method when found relevant.
-   * </p>
-   *
-   * <p>Null Implementation:</p>
-   * <pre>
-   * return null;
-   * </pre>
-   *
-   * @param item new item to set
-   * @param path path to follow
-   * @return newly set item or null if this path is not supported or the data structure is immutable
-   */
-  public Object put(Object path, Object item);
+   * @param shower TokyoNaut instance communicating through gesture
+   * @param towel communicated thing
+   */ 
+  public void show(ITokyoNaut shower, Object towel);
   
 }
