940e087e6527f57313ae799aec2a4d59941be6d7
Eric Bréchemier <github@eric.brechemier.name>
2007-01-11 22:12:12 +0000

    Deeply simplified API and tests for Harajuku release

--- main/src/java/net/sf/tokyo/ITokyoNaut.java
+++ main/src/java/net/sf/tokyo/ITokyoNaut.java
@@ -2,7 +2,7 @@
  * The Tokyo Project is hosted on Sourceforge:
  * http://sourceforge.net/projects/tokyo/
  * 
- * Copyright (c) 2005-2006 Eric Bréchemier
+ * Copyright (c) 2005-2007 Eric Bréchemier
  * http://eric.brechemier.name
  * 
  * This library is free software; you can redistribute it and/or
@@ -22,107 +22,56 @@
 package net.sf.tokyo;
 
 /**
- * ITokyoNaut is the one single interface making up TokyoNautilus API<br />
+ * ITokyoNaut is the one single interface making up Tokyo API.<br/>
  *
- * The TokyoNautilus API enables both XML and non-XML data browsing through a concise API. Nautilus is a 
- * reference to Captain Nemo's submarine in "Twenty Thousand Leagues Under the Sea" by Jules Verne.
+ * <p>
+ * The name "TokyoNaut" is composed of "Tokyo" which inspired this project, while "Naut" is a shorthand 
+ * for Nautilus, Captain Nemo's submarine in "Twenty Thousand Leagues Under the Sea" by Jules Verne. 
  * Captain Nemo's motto, "Mobilis in Mobili", fits well with this flexible data navigation API.
+ * </p>
+ *
+ * <p>
+ * TokyoNaut instances (classes that implement the ITokyoNaut interface) are expected to be stateless. 
+ * Each call of "morph" is intended to rewrite the input data by interpreting a set of instructions 
+ * or "rules", one step at a time, and updating the associated state for use in following runs. 
+ * While "morph" modifies the data, the state, and even possibly the rules, "remain" is read only and 
+ * returns an integer value giving an (optimistic) assessment of remaining steps.
+ * </p>
  * 
  * @author Eric Br&eacute;chemier
- * @version Shibuya
+ * @version Harajuku
  */
 public interface ITokyoNaut
 {
   
   /**
-   * Every data processing starts with such a meeting between two TokyoNaut instances.<br/>
+   * Assess Remaining Steps in an optimistic way.<br/>
    *
    * <p>
-   * The stranger instance is to propose a language stored in context[language], with
-   * type[language] characterizing the language Object using a shared convention. 
-   * The hearer TokyoNaut may either say something using the language, or keep silent 
-   * and quit, using the same context, type and language as arguments.
-   * </p>
-   * <p>
-   *  <b>Note:</b> TokyoNaut API makes extensive use of the three parameters <br/>
-   *  Object[] context + int[] type + int someObjectPosition. <br/>
-   *  The combination of context and type can be seen as a stack of named arguments,
-   *  while the last integer parameter points to the top of the stack. 
-   *  The rule is rather to keep the same arrays for the duration of the conversation,
-   *  even if bigger arrays may be introduced as required if shared conventions associated 
-   *  with the current language allow it. Besides, the two arrays should have the same size.  
+   * The optimistic evaluation will always return a number lower or equal to the number
+   * of remaining steps before the completion of the task at hand as expressed by rules.
    * </p>
    *
-   * @param stranger TokyoNaut instance coming to start a conversation
-   * @param context stack of questions and answers
-   * @param type array of codes identifying types of objects at the same position in context array
-   * @param language position of language object in context array
-   */
-  public void meet(ITokyoNaut stranger, Object[] context, int[] type, int language);
-  
-  /**
-   * At the end of all discussions, TokyoNauts part company.<br/>
-   *
-   * <p>
-   * This is the occasion
-   * to free contextual ressources related to the task. Any TokyoNaut may end the discussion,
-   * not necessarily the one having started it by calling the meet method.
-   * context[language] and type[language] will usually be identical to the values
-   * provided in the initial meet method.
-   * </p>
-   *
-   * @param friend TokyoNaut instance wishing to quit the conversation
-   * @param context stack of questions and answers
-   * @param type array of codes identifying types of objects at the same position in context array
-   * @param language position of language object in context array
-   */
-  public void quit(ITokyoNaut friend, Object[] context, int[] type, int language);
-  
-  /**
-   * TokyoNauts instances communicate by calling alternatively this "say" method.<br/>
-   *
-   * <p>
-   * The speaker TokyoNaut says context[sentence], characterized by type[sentence], 
-   * assuming it is relevant in the context. 
-   * Each TokyoNaut is free to update the context stack to keep only the most relevant items. 
-   * For hierarchical data processing, the context may for example contain the stack of
-   * ancestors on the path from the root to the current node.
-   * </p>
-   *
-   * @param speaker TokyoNaut instance uttering the sentence
-   * @param context stack of questions and answers
-   * @param type array of codes identifying types of objects at the same position in context array
-   * @param sentence position of sentence object in context array
-   */
-  public void say(ITokyoNaut speaker, Object[] context, int[] type, int sentence);
-  
-  /**
-   * When tired of speaking, TokyoNauts can enjoy a little distraction...<br/>
-   *
-   * <p>
-   * ... by playing
-   * a quick game, a piece of music or even theater, proposed as context[play]. 
-   * Serious TokyoNauts can freely skip the implementation of this method.
-   * </p>
-   *
-   * @param player TokyoNaut instance communicating through play
-   * @param context stack of questions and answers and various plays
-   * @param type array of codes identifying types of objects at the same position in context array
-   * @param play position of play object in context array
+   * @param rules list of rewrite operations (read only)
+   * @param state current state resulting from previous morph operation (read only)
+   * @param values current values (read only)
+   * @return optimistic evaluation of remaining steps (subsequent calls of morph required to complete the operation)
    */ 
-  public void play(ITokyoNaut player, Object[] context, int[] type, int play);
+  public int remain(Object[] rules, Object[] state, Object[] values);
   
   /**
-   * No need to panic, says Japan coach.<br/>
+   * Apply one step of rewrite operations to data.<br/>
    *
    * <p>
-   * Gestures provide alternative communication, especially useful for logs, 
-   * assertions and errors.
+   * Rules are interpreted by the TokyoNaut to rewrite the values, one step at a time.
+   * The state param is a useful storage for the internal state of the processing (if needed)
+   * in order to keep the operation stateless.
    * </p>
    *
-   * @param shower TokyoNaut instance communicating through gesture
-   * @param towel communicated thing
-   */ 
-  public void show(ITokyoNaut shower, Object towel);
+   * @param rules list of rewrite operations (read/write)
+   * @param state current state resulting from previous morph operation (read/write)
+   * @param values current values (read/write)
+   */
+  public void morph(Object[] rules, Object[] state, Object[] values);
   
 }
---
+++ main/test/java/net/sf/tokyo/test/NullTokyoNautTest.java
@@ -0,0 +1,44 @@
+/*
+ * The Tokyo Project is hosted on Sourceforge:
+ * http://sourceforge.net/projects/tokyo/
+ * 
+ * Copyright (c) 2005-2007 Eric Bréchemier
+ * http://eric.brechemier.name
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+package net.sf.tokyo.test;
+
+import net.sf.tokyo.ITokyoNaut;
+
+/**
+ * Simple coverage test of Tokyo API based on NullTokyoNaut.
+ */
+public class NullTokyoNautTest
+{
+  public static void main(String[] args)
+  {
+    Object[] rules = {"http://tokyo.sf.net/null/rules","do nothing"};
+    Object[] state = {};
+    Object[] values = {};
+    
+    ITokyoNaut tokyoNaut = new NullTokyoNaut();
+    while( tokyoNaut.remain(rules,state,values) >0 ) 
+    {
+      tokyoNaut.morph(rules,state,values);
+    }
+    System.out.println("Tokyo API coverage test complete.");
+  }
+}
