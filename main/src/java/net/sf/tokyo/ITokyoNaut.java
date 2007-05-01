be0bbfd25b9c8f103c303148824a25cbe2972551
Eric Bréchemier <github@eric.brechemier.name>
2007-05-01 13:43:25 +0000

    modified API to clarify the interactions of associated TokyoNauts involved in the different steps of a transformation

--- main/src/java/net/sf/tokyo/ITokyoNaut.java
+++ main/src/java/net/sf/tokyo/ITokyoNaut.java
@@ -22,16 +22,17 @@
 package net.sf.tokyo;
 
 /**
- * ITokyoNaut is the single interface making up Tokyo API.<br/>
+ * ITokyoNaut is the single interface making up Tokyo API, 
+ * allowing both XML and non-XML data transformations.<br/>
  *
  * <p>
- * The name "TokyoNaut" is composed of "Tokyo" which inspired this project, while "Naut" is a shorthand 
- * for Nautilus, Captain Nemo's submarine in "Twenty Thousand Leagues Under the Sea" by Jules Verne. 
- * Captain Nemo's motto, "Mobilis in Mobili", fits well with this flexible data navigation API.
+ * The name "TokyoNaut" is composed of "Tokyo", a mind-changing city where this project started,
+ * while "Naut" is a shorthand reference to Nautilus, Captain Nemo's submarine in Jules Verne's
+ * "Twenty Thousand Leagues Under the Sea".
+ * More inspiration comes from Captain Nemo's motto: "Mobilis in Mobili".
  * </p>
  *
  * <p>
- * TokyoNaut instances (classes that implement the ITokyoNaut interface) are expected to be stateless. 
  * Each call of "morph" is intended to rewrite the input data by interpreting a set of instructions 
  * or "rules", one step at a time, and updating the associated state for use in following runs. 
  * </p>
@@ -43,18 +44,67 @@ public interface ITokyoNaut
 {
   
   /**
-   * Rewrite data according to rules and update state.<br/>
+   * Rewrite data according to rules based on atomic transformations (TokyoNauts).<br/>
    *
    * <p>
-   * Rules are interpreted by the TokyoNaut to rewrite the values, one step at a time.
-   * While the internal state required for processing is stored in boxes of the state array,
-   * ITokyoNaut instances remain stateless.
+   * Each call of the "morph" method runs one single rewrite step, which could correspond to 
+   * one line of text, one binary structure or an XML fragment depending on the format at hand 
+   * and the choice of implementation.
    * </p>
    *
-   * @param state current state resulting from previous morph operation
-   * @param rules list of rewrite operations
-   * @param data current values
+   * <p>
+   * A set of simple TokyoNaut instances may be associated to realize a complex data transformation,
+   * each one located in a separate row of the "rules" array.
+   * </p>
+   *
+   * <p>
+   * "state" and "data" have the same number of elements as "rules". The association of these three 
+   * arrays can be seen as a table where the row of index "i" is made of (state[i],rules[i],data[i]).
+   * </p>
+   *
+   * <p>
+   * "index" is a single element array (int[1]) providing current index as a read/write parameter.
+   * The current index is the position of the current TokyoNaut, which will execute its atomic morph 
+   * operation in the run of the morph call.
+   * </p>
+   *
+   * <p>
+   * Current TokyoNaut takes data as input in data[index[0]] and applies a transformation resulting to 
+   * new data as output stored in data[index[1]], provided as input for the following transformation.
+   * First transformation would typically take input data from a file or a data input stream, while
+   * last transformation would typically store output data in a file or a data output stream.
+   * </p>
+   *
+   * <p> 
+   * The returned TokyoNaut instance is the next to be called to continue the transformation process,
+   * or null if the transformation is complete (in which case index[0] shall be set to rules.length).
+   * </p>
+   * 
+   * <p>
+   * Given that state, rules and data have been properly initialized, the following loop would run
+   * a step by step transformation until completion:
+<PRE>
+int[] index = {0};
+ITokyoNaut current = rules[index[0]];
+while (current != null)
+{
+  current = current.morph(state,rules,data,index);
+}     
+</PRE>
+   * </p>
+   * 
+   * <p>
+   * In the above example, it is not possible to replace the "while loop" with a "for loop",
+   * because the flow of morph operations will typically loop back and forth from data providers
+   * to data consumers, involding several passes trough the same TokyoNauts until completion.
+   * </p>
+   *
+   * @param state initial configuration and current state of atomic transformations
+   * @param rules chain of atomic transformations (TokyoNauts)
+   * @param data buffers of data for atomic transformations
+   * @param index position of current TokyoNaut, identical in the three arrays state,rules,data
+   * @return next TokyoNaut to run i.e. rules[index[0]] or null when transformation is complete
    */
-  public void morph(Object[] state, Object[] rules, Object[] data);
+  public ITokyoNaut morph(Object[] state, ITokyoNaut[] rules, Object[] data, int[] index);
   
 }
