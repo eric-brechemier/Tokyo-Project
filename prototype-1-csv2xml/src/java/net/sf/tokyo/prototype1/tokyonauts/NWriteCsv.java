3458b642dcf64ad973b72c4f3b35b509d7462ec3
Eric Bréchemier <github@eric.brechemier.name>
2007-08-23 00:48:08 +0000

    completed non-optimized version

--- prototype-1-csv2xml/src/java/net/sf/tokyo/prototype1/tokyonauts/NWriteCsv.java
+++ prototype-1-csv2xml/src/java/net/sf/tokyo/prototype1/tokyonauts/NWriteCsv.java
@@ -24,29 +24,135 @@ package net.sf.tokyo.prototype1.tokyonauts;
 
 import net.sf.tokyo.ITokyoNaut;
 
+import java.io.UnsupportedEncodingException;
 
 /**
- * Write XML data corresponding to a CSV structure into a byte array.<br/>
+ * Write Tokyo Project events to CSV.<br/>
  */
-public abstract class NWriteCsv implements ITokyoNaut
+public class NWriteCsv extends NCommonBase implements ITokyoNaut
 {
-  public boolean inTouch()
+  protected boolean _completed;
+  protected String _charEncoding;
+  protected int _lineNumber;
+  protected int _fieldNumber;
+  
+  public NWriteCsv()
   {
-    return false;
+    _completed = false;
+    _charEncoding = "UTF-8";
+    _lineNumber = 1;
+    _fieldNumber = 1;
   }
   
-  public void read(int[]meta, byte[] data)
+  public boolean areWeThereYet()
   {
-    return;
+    return _completed;
   }
   
-  public ITokyoNaut plug(ITokyoNaut source)
+  public void filter(int[]meta, byte[] data)
   {
-    return source;
+    if(areWeThereYet() || meta[VERSION]!=VERSION_ONE)
+      return;
+    
+    switch(meta[ITEM])
+    {
+      case ITEM_DOCUMENT:
+        _completed = (meta[EVENT]==END);
+        super.filter(meta,data);
+        break;
+      
+      case ITEM_CHARACTERS_CODE:
+        meta[ITEM] = ITEM_DOCUMENT;
+        meta[EVENT] = CONTINUATION;
+        super.filter(meta,data);
+        break;
+      
+      case ITEM_DOCUMENT_CHARACTER_ENCODING:
+        String charEncodingPart=null;
+        try
+        {
+          charEncodingPart = new String(data,meta[OFFSET],meta[LENGTH],"UTF-8");
+        }
+        catch(UnsupportedEncodingException e)
+        {
+          // TODO: signal error with event
+          System.out.println("ERROR: "+e);
+        }
+        if (meta[EVENT]==START)
+          _charEncoding = charEncodingPart;
+        else if (meta[EVENT]==CONTINUATION)
+          _charEncoding = _charEncoding + charEncodingPart;
+        break;
+        
+      // case NAMESPACE...
+      // TODO: consider namespace URI as well
+      
+      case ITEM_ELEMENT_LOCAL_NAME:
+        
+        // TODO: consider case with CONTINUATION for localName
+        if (meta[EVENT]==START)
+        {
+          String strLocalName=null;
+          try
+          {
+            strLocalName = new String(data,meta[OFFSET],meta[LENGTH],_charEncoding);
+          }
+          catch(UnsupportedEncodingException e)
+          {
+            // TODO: signal error with event
+            System.out.println("ERROR: "+e);
+          }
+          
+          meta[ITEM] = ITEM_DOCUMENT;
+          
+          // if "file" nothing to do
+          if ("line".equals(strLocalName))
+          {
+            if (_lineNumber>1)
+            {
+              meta[ITEM] = ITEM_DOCUMENT;
+              meta[EVENT] = CONTINUATION;
+              meta[OFFSET] = 0;
+              meta[LENGTH] = 2;
+              data[0] = '\r';
+              data[1] = '\n';
+              super.filter(meta,data);
+            }
+            _lineNumber ++;
+            _fieldNumber = 1;
+          }
+          else if ("field".equals(strLocalName))
+          {
+            if (_fieldNumber>1)
+            {
+              meta[ITEM] = ITEM_DOCUMENT;
+              meta[EVENT] = CONTINUATION;
+              meta[OFFSET] = 0;
+              meta[LENGTH] = 1;
+              data[0] = ',';
+              super.filter(meta,data);
+            }
+            _fieldNumber++;
+          }
+          else
+            System.out.println("Unexpected element: "+strLocalName);
+        }
+        break;
+      
+      default:
+        // no event
+        
+    }
+    
+  }
+  
+  public ITokyoNaut plug(ITokyoNaut destination)
+  {
+    return super.plug(destination);
   }
   
   public void unplug()
   {
-    
+    super.unplug();
   }
 }
