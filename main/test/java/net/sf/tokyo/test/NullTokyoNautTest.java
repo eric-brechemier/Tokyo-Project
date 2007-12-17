3f561eb5f02ed59627e241a9151b4b39cc853fcb
Eric Bréchemier <github@eric.brechemier.name>
2007-12-17 12:05:20 +0000

    Main API Test Coverage: [ADD] border condition to limit the number of execution steps in the main loop

--- main/test/java/net/sf/tokyo/test/NullTokyoNautTest.java
+++ main/test/java/net/sf/tokyo/test/NullTokyoNautTest.java
@@ -30,18 +30,108 @@ public class NullTokyoNautTest
 {
   public static void main(String[] args)
   {
-    ITokyoNaut nullTokyoNaut = new NullTokyoNaut();
+    ITokyoNaut nullTokyoNautSrc = new NullTokyoNaut();
     ITokyoNaut nullTokyoNautDst = new NullTokyoNaut();
-    int[] meta = new int[]{1,0,0,0,0};
     byte[] data = new byte[500];
+    int[] meta 
+      = new int[]
+      {
+        ITokyoNaut.VERSION_NANA,
+        ITokyoNaut.LANGUAGE_BINARY,
+        ITokyoNaut.TOKEN_SPARK,
+        ITokyoNaut.LEFT_START,
+        0, 
+        0,
+        ITokyoNaut.RIGHT_END
+      };
     
-    nullTokyoNaut.plug(nullTokyoNautDst);
+    nullTokyoNautSrc.plug(nullTokyoNautDst);
     
-    do nullTokyoNaut.filter(meta,data);
-    while ( !nullTokyoNaut.areWeThereYet() );
+    int step = 0;
+    final int STEP_LIMIT = 3;
+    while( !nullTokyoNautDst.areWeThereYet(meta,data) && (step++ < STEP_LIMIT)  )
+    {
+      if ( meta[ITokyoNaut.VERSION] == ITokyoNaut.VERSION_NANA)
+      {
+        System.out.println
+          (  
+             "Step: "+step+"\n\t"
+            +"Data Item:\n\t"
+            +"Language: "+getLanguageName(meta[ITokyoNaut.LANGUAGE])+"\n\t"
+            +"Token: "+getTokenName(meta[ITokyoNaut.TOKEN])+"\n\t"
+            +"Left Relation: "+getRelationName(meta[ITokyoNaut.LEFT])+"\n\t"
+            +"Fragment Offset: "+formatOffset(meta[ITokyoNaut.OFFSET])+"\n\t"
+            +"Fragment Length: "+formatLength(meta[ITokyoNaut.LENGTH])+"\n\t"
+            +"Right Relation: "+getRelationName(meta[ITokyoNaut.RIGHT])
+          );
+      }
+    }
     
-    nullTokyoNaut.unplug();
+    nullTokyoNautSrc.unplug(nullTokyoNautDst);
     
     System.out.println("Tokyo API coverage test complete.");
   }
+  
+  public static String getLanguageName(int language)
+  {
+    switch(language)
+    {
+      case ITokyoNaut.LANGUAGE_BINARY:
+        return "Binary";
+      
+      case ITokyoNaut.LANGUAGE_UNICODE_TEXT:
+        return "ERROR";
+      
+      case ITokyoNaut.LANGUAGE_ERROR:
+        return "ERROR";
+        
+      default:
+        return "Other Language ["+language+"]";
+    }
+  }
+  
+  public static String getTokenName(int token)
+  {
+    switch(token)
+    {
+      case ITokyoNaut.TOKEN_SPARK:
+        return "Spark to Start!";
+      
+      case ITokyoNaut.TOKEN_BINARY:
+        return "Binary";
+      
+      default:
+        return "Other Token ["+token+"]";
+    }
+  }
+  
+  public static String getRelationName(int relation)
+  {
+    switch(relation)
+    {
+      case ITokyoNaut.LEFT_START:
+        return "Token Start";
+        
+      case ITokyoNaut.LEFT_CONTINUED:
+      //case ITokyoNaut.RIGHT_CONTINUED:
+        return "Token Continued";
+        
+      case ITokyoNaut.RIGHT_END:
+        return "Token End";
+        
+      default:
+        return "Unknown Relationship!";
+    }
+  }
+  
+  public static String formatOffset(int offset)
+  {
+    return "Ox"+Integer.toHexString(offset);
+  }
+  
+  public static String formatLength(int length)
+  {
+    return length+(length>=2?" bytes":" byte");
+  }
+  
 }
