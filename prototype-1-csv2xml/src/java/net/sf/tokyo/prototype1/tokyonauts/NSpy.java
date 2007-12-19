a70737b1540fedc3b68c9e22dc393d6d7c6bbe70
Eric Bréchemier <github@eric.brechemier.name>
2007-12-19 14:11:55 +0000

    Tokyo Project Prototype 1: Spy TokyoNaut now under License MIT/BSD

--- prototype-1-csv2xml/src/java/net/sf/tokyo/prototype1/tokyonauts/NSpy.java
+++ prototype-1-csv2xml/src/java/net/sf/tokyo/prototype1/tokyonauts/NSpy.java
@@ -1,25 +1,41 @@
-/*
- * The Tokyo Project is hosted on Sourceforge:
- * http://sourceforge.net/projects/tokyo/
- * 
- * Copyright (c) 2005-2007 Eric Bréchemier
- * http://eric.brechemier.name
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- */
+/* ===============================================================
+ The Tokyo Project is hosted on Sourceforge:
+ http://sourceforge.net/projects/tokyo/
+ 
+ Copyright (c) 2005-2007 Eric Bréchemier
+ http://eric.brechemier.name
+ Licensed under BSD License and/or MIT License.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                         BSD License
+                             ~~~
+             http://creativecommons.org/licenses/BSD/
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                          MIT License
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Copyright (c) 2005-2007 Eric Bréchemier <tokyo@eric.brechemier.name>
+  
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+================================================================== */
 package net.sf.tokyo.prototype1.tokyonauts;
 
 import java.util.Enumeration;
@@ -37,17 +53,12 @@ public class NSpy extends NCommonBase implements ITokyoNaut
   public static final byte STYLE_DEFAULT = STYLE_HEX;
   
   protected byte _logStyle;
-  
-  protected int _level;
   protected Stack _contextPath;
-  protected boolean _completed;
   
   public NSpy(byte logStyle)
   {
     _logStyle = logStyle;
-    _level = -1;
     _contextPath = new Stack();
-    _completed = false;
   }
   
   public NSpy()
@@ -55,49 +66,48 @@ public class NSpy extends NCommonBase implements ITokyoNaut
     this(STYLE_DEFAULT);
   }
   
-  public boolean areWeThereYet()
+  public boolean areWeThereYet(int[]meta, byte[] data)
   {
-    return _completed;
-  }
-  
-  public void filter(int[]meta, byte[] data)
-  {
-    try 
-    {
-      printSeparator();
-      
-      if (meta[VERSION] != VERSION_ONE)
-        System.out.println("Spy> !!! WARNING: Unsupported version: "+VERSION_ONE);
-      
-      if (meta[EVENT]==END && meta[ITEM]==ITEM_DOCUMENT)
-        _completed = true;
-      
-      _level += meta[EVENT];
-      printContextStack(meta);
-      
-      printDataBuffer(data);
-      
-      printSeparator();
-      
-      long nsBefore = System.nanoTime();
-      super.filter(meta,data);
-      long nsAfter = System.nanoTime();
-      printTimeDifference(nsAfter,nsBefore);
-      
-      printSeparator();
-    }
-    catch(Exception e)
-    {
-      System.err.println("Error in NSpy.filter(): "+e);
-      e.printStackTrace();
-      return;
-    }
+    if (meta[VERSION] != ITokyoNaut.VERSION_NANA)
+        System.out.println("Spy> !!! WARNING: Unsupported version: "+meta[VERSION]);
+    
+    if (_src==null)
+      return true;
+    
+    printSeparator();
+    long nsBefore = System.nanoTime();
+    boolean result = _src.areWeThereYet(meta,data);
+    long nsAfter = System.nanoTime();
+    printTimeDifference(nsAfter,nsBefore);
+    
+    System.out.println("Spy> VERSION:  "+meta[ITokyoNaut.VERSION]);
+    System.out.println("Spy> LANGUAGE: "+meta[ITokyoNaut.LANGUAGE]);
+    System.out.println("Spy> TOKEN:    "+meta[ITokyoNaut.TOKEN]);
+    System.out.println("Spy> LEFT:    "+meta[ITokyoNaut.LEFT]);
+    System.out.println("Spy> OFFSET:  "+meta[ITokyoNaut.OFFSET]);
+    System.out.println("Spy> LENGTH:  "+meta[ITokyoNaut.LENGTH]);
+    System.out.println("Spy> RIGHT:   "+meta[ITokyoNaut.RIGHT]);
+    
+    printSeparator();
+    printDataBuffer(data);
+    printSeparator();
+    
+    return result;
   }
   
   protected void printSeparator()
   {
     System.out.println("Spy> ~~~ "+System.nanoTime());
   } 
+  
+  protected void printTimeDifference(long nsAfter, long nsBefore)
+  {
+    long microsDiff = (nsAfter-nsBefore)/1000;
+    long msDiff = microsDiff/1000;
+    int microsRemain =  (int)(microsDiff%1000);
+    System.out.println("Spy> Spying: "+_src
+      +" ("+msDiff+"."+formatDecNumber(microsRemain,3,'0')+"ms) ("+(nsAfter-nsBefore)+"ns)");  
+  }
   
   protected void printDataBuffer(byte[] data)
   {
@@ -133,6 +143,67 @@ public class NSpy extends NCommonBase implements ITokyoNaut
     }
     System.out.println("\n");
   }  
+  
+  protected String formatDecNumber(int value, int length, char padding)
+  {
+    String strValue = Integer.toString(value);
+    while(strValue.length() < length)
+      strValue = padding + strValue;
+    return strValue;
+  }
+  
+  protected String formatHexNumber(int value, int length, char padding)
+  {
+    String strValue = Integer.toHexString(value);
+    while(strValue.length() < length)
+      strValue = padding + strValue;
+    return strValue;
+  }
+  
+  protected String formatChar(char value)
+  {
+    if ( Character.isISOControl(value) )
+      return "\u229D"; // circled dash
+    else
+      return ""+value;
+  }  
+  
+  
+  /*
+  public void translate(int[]meta, byte[] data)
+  {
+    try 
+    {
+      printSeparator();
+      
+      if (meta[VERSION] != VERSION_ONE)
+        System.out.println("Spy> !!! WARNING: Unsupported version: "+VERSION_ONE);
+      
+      if (meta[EVENT]==END && meta[ITEM]==ITEM_DOCUMENT)
+        _completed = true;
+      
+      _level += meta[EVENT];
+      printContextStack(meta);
+      
+      printDataBuffer(data);
+      
+      printSeparator();
+      
+      long nsBefore = System.nanoTime();
+      super.translate(meta,data);
+      long nsAfter = System.nanoTime();
+      printTimeDifference(nsAfter,nsBefore);
+      
+      printSeparator();
+    }
+    catch(Exception e)
+    {
+      System.err.println("Error in NSpy.filter(): "+e);
+      e.printStackTrace();
+      return;
+    }
+  }
+  
   
   protected void printContextStack(int[] meta)
   {  
@@ -173,38 +244,7 @@ public class NSpy extends NCommonBase implements ITokyoNaut
     System.out.print("\n");
   }
   
-  protected void printTimeDifference(long nsAfter, long nsBefore)
-  {
-    long microsDiff = (nsAfter-nsBefore)/1000;
-    long msDiff = microsDiff/1000;
-    int microsRemain =  (int)(microsDiff%1000);
-    System.out.println("Spy> Spying: "+_destination
-      +" ("+msDiff+"."+formatDecNumber(microsRemain,3,'0')+"ms) ("+(nsAfter-nsBefore)+"ns)");  
-  }
   
-  protected String formatDecNumber(int value, int length, char padding)
-  {
-    String strValue = Integer.toString(value);
-    while(strValue.length() < length)
-      strValue = padding + strValue;
-    return strValue;
-  }
-  
-  protected String formatHexNumber(int value, int length, char padding)
-  {
-    String strValue = Integer.toHexString(value);
-    while(strValue.length() < length)
-      strValue = padding + strValue;
-    return strValue;
-  }
-  
-  protected String formatChar(char value)
-  {
-    if ( Character.isISOControl(value) )
-      return "\u229D"; // circled dash
-    else
-      return ""+value;
-  }  
   
   protected String formatType(int itemType)
   {
@@ -275,14 +315,6 @@ public class NSpy extends NCommonBase implements ITokyoNaut
     }
   }
   
-  public ITokyoNaut plug(ITokyoNaut destination)
-  {
-    return super.plug(destination);
-  }
-  
-  public void unplug()
-  {
-    super.unplug(); 
-  }
+  */
   
 }
